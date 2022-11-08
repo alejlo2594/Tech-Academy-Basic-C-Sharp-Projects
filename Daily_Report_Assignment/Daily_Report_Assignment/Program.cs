5883e3afd22ba4483d0d169ed97253211c1a2d52
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-07 23:15:06 -0500

    Updated Daily_Report_Assignment

---
+++ Daily_Report_Assignment/Daily_Report_Assignment/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Report_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opening sentence
+            Console.WriteLine("The Tech Academy");
+            Console.ReadLine();
+
+            //Student Daily Report
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+
+            //Question A 
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourName);
+            Console.ReadLine();
+
+            //Question B
+            Console.WriteLine("What is course are you on?");
+            string yourCourse = Console.ReadLine();
+            Console.WriteLine("You are currently on the " + yourCourse + " course!");
+            Console.ReadLine();
+
+            //Question C 
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            Console.WriteLine("You are on page " + pageNumber);
+            Console.ReadLine();
+
+            //Question D
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            Console.ReadLine();
+            Console.WriteLine("Thank you for your response");
+            Console.ReadLine();
+
+            //Question E
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            Console.ReadLine();
+            Console.WriteLine("Thank you for your response");
+            Console.ReadLine();
+
+            //Question F
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            Console.ReadLine();
+            Console.WriteLine("Thank you for your feedback");
+            Console.ReadLine();
+
+            //Question G
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            Console.WriteLine("You studied for " + hoursStudied + " hours today!");
+            Console.ReadLine();
+
+            //Last response
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
--- VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -22,6 +22,29 @@ namespace VariablesAndDataTypes
             int total = favNum + 5;
             Console.WriteLine("Your favorite number plus 5 is: " + total);
             Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Jesse";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
         }
     }
 }
