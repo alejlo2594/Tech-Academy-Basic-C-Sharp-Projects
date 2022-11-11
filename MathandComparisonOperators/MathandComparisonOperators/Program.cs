97de1fef0ba1cc2c688e600aa44cc1d6fb3bbada
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-10 22:41:57 -0500

    Updated

--- MathandComparisonOperators/MathandComparisonOperators/Program.cs
+++ MathandComparisonOperators/MathandComparisonOperators/Program.cs
@@ -28,9 +28,24 @@ namespace MathandComparisonOperators
             //Console.WriteLine(quotient);
             //Console.ReadLine();
 
-            int remainder = 11 % 2;
-            Console.WriteLine(remainder);
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemperature = 70;
+            int currentTemperature = 70;
+
+            //bool isWarm = currentTemperature <= roomTemperature;
+            bool isWarm = currentTemperature != roomTemperature;
+            Console.WriteLine(isWarm);
             Console.ReadLine();
+
+
+
         }
     }
 }
