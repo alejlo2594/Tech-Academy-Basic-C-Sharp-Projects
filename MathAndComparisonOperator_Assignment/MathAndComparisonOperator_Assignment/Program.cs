bc11d3e897225b8e37742263e5b2ec14f56642d8
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-10 23:47:01 -0500

    Updated

--- MathAndComparisonOperator_Assignment/MathAndComparisonOperator_Assignment/Program.cs
+++ MathAndComparisonOperator_Assignment/MathAndComparisonOperator_Assignment/Program.cs
@@ -10,6 +10,58 @@ namespace MathAndComparisonOperator_Assignment
     {
         static void Main(string[] args)
         {
+            //Opening Title
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+
+            //Person 1
+            Console.WriteLine("Person 1");
+            Console.ReadLine();
+            Console.WriteLine("Hourly Rate?");
+            Console.ReadLine();
+            int person1Rate = 15;
+            Console.WriteLine(person1Rate);
+            Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            Console.ReadLine();
+            int person1Hours = 40;
+            Console.WriteLine(person1Hours);
+            Console.ReadLine();
+
+            //Person 2
+            Console.WriteLine("Person 2");
+            Console.ReadLine();
+            Console.WriteLine("Hourly Rate?");
+            Console.ReadLine();
+            int person2Rate = 20;
+            Console.WriteLine(person2Rate);
+            Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            Console.ReadLine();
+            int person2Hours = 40;
+            Console.WriteLine(person2Hours);
+            Console.ReadLine();
+
+            //Annual Salary of Person 1
+            Console.WriteLine("Annual Salary of Person 1: ");
+            Console.ReadLine();
+            int product1 = person1Rate * person1Hours * 52;
+            Console.WriteLine(product1);
+            Console.ReadLine();
+
+            //Annual Salary of Person 2
+            Console.WriteLine("Annual Salary of Person 2: ");
+            Console.ReadLine();
+            int product2 = person2Rate * person2Hours * 52;
+            Console.WriteLine(product2);
+            Console.ReadLine();
+
+            //Last Question
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.ReadLine();
+            bool isMore = product1 > product2;
+            Console.WriteLine(isMore);
+            Console.ReadLine();
         }
     }
 }
