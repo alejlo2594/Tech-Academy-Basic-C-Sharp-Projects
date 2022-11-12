adce7ba195f90a25067df350740be9a2f836358e
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-11 22:41:53 -0500

    Updated

--- MathAndComparisonOperator_Assignment/MathAndComparisonOperator_Assignment/Program.cs
+++ MathAndComparisonOperator_Assignment/MathAndComparisonOperator_Assignment/Program.cs
@@ -16,51 +16,30 @@ namespace MathAndComparisonOperator_Assignment
 
             //Person 1
             Console.WriteLine("Person 1");
-            Console.ReadLine();
             Console.WriteLine("Hourly Rate?");
-            Console.ReadLine();
-            int person1Rate = 15;
-            Console.WriteLine(person1Rate);
-            Console.ReadLine();
+            int person1Rate = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hours worked per week?");
-            Console.ReadLine();
-            int person1Hours = 40;
-            Console.WriteLine(person1Hours);
-            Console.ReadLine();
+            int person1Hours = Convert.ToInt32(Console.ReadLine());
 
             //Person 2
             Console.WriteLine("Person 2");
-            Console.ReadLine();
             Console.WriteLine("Hourly Rate?");
-            Console.ReadLine();
-            int person2Rate = 20;
-            Console.WriteLine(person2Rate);
-            Console.ReadLine();
+            int person2Rate = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hours worked per week?");
-            Console.ReadLine();
-            int person2Hours = 40;
-            Console.WriteLine(person2Hours);
-            Console.ReadLine();
+            int person2Hours = Convert.ToInt32(Console.ReadLine());
 
             //Annual Salary of Person 1
-            Console.WriteLine("Annual Salary of Person 1: ");
-            Console.ReadLine();
-            int product1 = person1Rate * person1Hours * 52;
-            Console.WriteLine(product1);
-            Console.ReadLine();
+            string annualSalary1 = Convert.ToString(person1Rate * person1Hours * 52);
+            Console.WriteLine("Annual Salary of Person 1: " + annualSalary1);
+
 
             //Annual Salary of Person 2
-            Console.WriteLine("Annual Salary of Person 2: ");
-            Console.ReadLine();
-            int product2 = person2Rate * person2Hours * 52;
-            Console.WriteLine(product2);
-            Console.ReadLine();
+            string annualSalary2 = Convert.ToString(person2Rate * person2Hours * 52);
+            Console.WriteLine("Annual Salary of Person 2: " + annualSalary2);
 
             //Last Question
-            Console.WriteLine("Does Person 1 make more money than Person 2?");
-            Console.ReadLine();
-            bool isMore = product1 > product2;
-            Console.WriteLine(isMore);
+            bool compareP1AndP2 = Convert.ToInt32(annualSalary1) > Convert.ToInt32(annualSalary2); 
+            Console.WriteLine("Does Person 1 make more money than Person 2? " + compareP1AndP2);
             Console.ReadLine();
         }
     }
