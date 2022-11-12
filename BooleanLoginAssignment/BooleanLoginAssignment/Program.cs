2799b55c5220403aecef94dde6f1be75b30b50a8
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-12 00:04:27 -0500

    Created BooleanLogicAssignment

---
+++ BooleanLoginAssignment/BooleanLoginAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLoginAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Title
+            Console.WriteLine("Basic Approval Program for Car Insurance: ");
+            //Questions
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool personDui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int personTickets = Convert.ToInt32(Console.Read());
+            bool qualifications = (age > 15 && personDui == false && personTickets <= 3);
+
+            //Qualifications
+            Console.WriteLine("Qualified? " + qualifications);
+            Console.ReadLine();
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ BooleanPractice/BooleanPractice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            Console.WriteLine(true ^ true);           //Will evaluate to true only if 1 is true not both
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
