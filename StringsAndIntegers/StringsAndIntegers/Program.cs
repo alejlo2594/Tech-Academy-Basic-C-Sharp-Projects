b5c218b1b2575d1fcaf447722285115d6baace2f
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-28 12:58:40 -0500

    Updated StringsAndIntegers

--- StringsAndIntegers/StringsAndIntegers/Program.cs
+++ StringsAndIntegers/StringsAndIntegers/Program.cs
@@ -10,12 +10,37 @@ namespace StringsAndIntegers
     {
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int> { 10, 20, 30, 40, 50 }; 
             try
             {
-                Console.WriteLine("Pick a number.");
-                int numberOne = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Pick a second number.");
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    Console.WriteLine("Pick a number.");
+                    int numberOne = Convert.ToInt32(Console.ReadLine());
+                    numbers[i] = numbers[i] / numberOne;
+                    Console.WriteLine(numbers[i]);
+                }
+                
+                
             }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            
         }
     }
 }
