e57af10fe1d93627ad0cb381e57e417faf448739
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-28 20:47:52 -0500

    Updated MethodsSubmission_Assignment

---
+++ MethodsSubmission_Assignment/MethodsSubmission_Assignment/Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsSubmission_Assignment
+{
+    class Methods
+    {
+        public int Add (int data, int value)
+        {
+            return data + value;
+        }
+        public int Subtract (int data, int value)
+        {
+            return data - value;
+        }
+        public int Multiply (int data, int value)
+        {
+            return data * value;
+        }
+    }
+}
---
+++ MethodsSubmission_Assignment/MethodsSubmission_Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsSubmission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            var userValue = Convert.ToInt32(Console.ReadLine());
+
+            var operatorObject = new Methods();
+            var result = operatorObject.Add(userValue, 10);
+            var result2 = operatorObject.Subtract(userValue, 2);
+            var result3 = operatorObject.Multiply(userValue, 15);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }                                                
+     }
+ }
+ 
+   
