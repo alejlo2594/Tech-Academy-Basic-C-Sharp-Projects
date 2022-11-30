f5728a83abe829e892fa70535a0b14af4bcca019
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-29 22:49:44 -0500

    Created MethodSubmissionPart3_Assignment

---
+++ MethodSubmissionPart3_ Assignment/MethodSubmissionPart3_ Assignment/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionPart3__Assignment
+{
+    class Methods
+    {
+        public void NewMethod(int num1, int num2)
+        {
+            int result = num1 + 75;
+            Console.WriteLine(num2);
+            Console.WriteLine(result);
+        }
+    }
+}
