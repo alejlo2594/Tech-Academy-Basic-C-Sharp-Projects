bc4c4b9da4cbca7fbd93214ddbcb67979e56345f
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-11-30 00:11:59 -0500

    Updated ClassSubmission_Assignment

---
+++ ClassSubmission_Assignment/ClassSubmission_Assignment/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission_Assignment
+{
+     public static class Methods
+     {
+         public static int number1(int num1, int num2)
+        {
+            int result = num1 / 2;
+            int result2 = num2 / 2;
+
+            Console.WriteLine("Number added divided by 2 equals: " + result2);
+            return result2;
+        }
+     }
+}
---
+++ ClassSubmission_Assignment/ClassSubmission_Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num1 = 10;
+            
+
+            Methods Operator = new Methods();
+            Operator.(num1, num2);
+
+            Console.ReadLine();
+        }
+    }
+}
