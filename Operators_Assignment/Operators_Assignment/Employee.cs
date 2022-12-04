95119b2dd3f32fa99eda060dedf96dd16fac2f64
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-12-04 11:41:33 -0500

    Created Operators_Assignment

---
+++ Operators_Assignment/Operators_Assignment/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int id { get; set; }
+        public Employee()
+        {
+            id = 0;
+        }
+
+        public Employee(int Employeeid1)
+        {
+            id = Employeeid1;
+        }
+
+        public static bool operator == (Employee Employeeid1, Employee Employeeid2)
+        {
+            return Employeeid1 == Employeeid2;
+        }
+
+        public static bool operator !=(Employee Employeeid1, Employee Employeeid2)
+        {
+            return Employeeid1 != Employeeid2;
+        }
+
+       
+
+    }
+}
---
+++ Operators_Assignment/Operators_Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> EmployeeList = new List<Employee>();
+            Employee Employeeid1 = new Employee();
+            Employeeid1.FirstName = "John";
+            Employeeid1.LastName = "Doe";
+            Employeeid1.id = 1;
+            EmployeeList.Add(Employeeid1);
+
+            Employee Employeeid2 = new Employee();
+            Employeeid2.FirstName = "Jane";
+            Employeeid2.LastName = "Dawson";
+            Employeeid2.id = 2;
+            EmployeeList.Add(Employeeid2);
+
+            Console.WriteLine(Employeeid1 == Employeeid2);
+            Console.WriteLine(Employeeid1 != Employeeid2);
+
+            Console.ReadLine();
+
+          
+        }
+    }
+}
