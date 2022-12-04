d1ce24b2f00c6f8cd3886a528b6f5316c6ab7eb8
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-12-04 14:08:29 -0500

    Created EnumsSubmission_Assignment

---
+++ EnumsSubmission_Assignment/EnumsSubmission_Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsSubmission_Assignment
+{
+    class Program
+    {
+        public enum DaysoftheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the current day of the week.");
+                string userDayoftheWeek = Console.ReadLine();
+
+                if (Enum.TryParse(userDayoftheWeek, out DaysoftheWeek numDay))
+                {
+                    Console.WriteLine("Today is " + numDay);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
