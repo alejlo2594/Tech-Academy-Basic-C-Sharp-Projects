a6dbffccd0fb235447273815dfa1be3248647b88
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-12-03 21:22:40 -0500

    Created MethodsAndObjects_Assignment

---
+++ MethodsAndObjects_Assignment/MethodsAndObjects_Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Person = new Employee();
+            Person.FirstName = ("Sample");
+            Person.LastName = ("Student");
+            Person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
