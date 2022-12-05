144eb505ac90d1d2ca2eb785bbdfcbf71fc87aa8
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-12-04 23:29:56 -0500

    Created UserDateAndTime_Assignment

---
+++ UserDateAndTime_Assignment/UserDateAndTime_Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UserDateAndTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+
+            Console.WriteLine("Please type a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            DateTime addTime = currentTime.AddHours(userNum);
+
+            Console.WriteLine(addTime);
+            Console.ReadLine();
+        }
+    }
+}
